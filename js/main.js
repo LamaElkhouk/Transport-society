219f2d971fb2bb40f491c3b73769f7c57a72d4ed
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2022-08-25 21:44:42 +0200

    section map finished for mobile and desktop version

--- js/main.js
+++ js/main.js
@@ -15,4 +15,36 @@ function BurgerToggle() {
     var menu = document.querySelectorAll(".element-menu");
     menu.forEach((el) => el.classList.toggle("showDesktop"));
 }
+//menu map toggle
+function MenuMapToggle() {
+
+    var agency = document.querySelectorAll(".agency");
+    agency.forEach((el) => el.classList.toggle("showDesktop"));
+}
+
+// Initialize and add the map
+function initMap() {
+    const map_icon = {
+        url: "../images/accueil/map_icon.svg",
+        scaledSize: new google.maps.Size(40, 40)
+    }
+    const agencies = [{ lat: -26.194810, lng: 28.279670 }, { lat: -29.761356, lng: 31.026772 }, { lat: -33.878100, lng: 18.698930 }]
+    // const Johannesburg = { lat: -26.194810, lng: 28.279670 }
+    // const Durban = { lat: -29.761356, lng: 31.026772 }
+    // const CapeTown = { lat: -33.878100, lng: 18.698930 }
+    const SA = { lat: -30.559483, lng: 22.937506 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: SA,
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+    agencies.forEach((agency) => new google.maps.Marker({
+        position: agency,
+        map: map,
+        icon: map_icon
+    }))
+}
+
+window.initMap = initMap;
 
